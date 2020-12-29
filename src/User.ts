e913194e79b88cb16143e11b3fd36f3ffbf854d6
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2020-12-30 04:03:19 +0900

    use identifier

--- src/User.ts
+++ src/User.ts
@@ -2,15 +2,27 @@ import { UserId } from './UserId';
 import { UserName } from './UserName';
 
 export class User {
-  id: UserId;
+  readonly id: UserId;
   name: UserName;
 
-  constructor(name: UserName) {
-    this.id = new UserId('dummy');
+  constructor(id: UserId, name: UserName) {
+    if (id === null) {
+      throw new Error('Id is null.');
+    }
+
+    if (name === null) {
+      throw new Error('Name is null.');
+    }
+
+    this.id = id;
     this.name = name;
   }
 
   changeName(name: UserName): void {
     this.name = name;
+  }
+
+  equals(other: User): boolean {
+    return this.id === other.id;
   }
 }
