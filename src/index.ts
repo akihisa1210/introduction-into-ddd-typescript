0778c4cbe9091f3391b6b4750bf71d4432dddc27
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-01-10 07:59:28 +0900

    mongodb: use try...catch

--- src/index.ts
+++ src/index.ts
@@ -27,27 +27,42 @@ const main = async () => {
     UserRegisterService,
   );
   const userRegistercommand = new UserRegisterCommand('NewUser');
-  await userRegisterService.handle(userRegistercommand);
+
+  try {
+    await userRegisterService.handle(userRegistercommand);
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log('---UserGetInfoService---');
+  const userGetInfoService: UserGetInfoService = container.resolve(
+    UserGetInfoService,
+  );
+  userGetInfoService.handle('1');
+
+  console.log('---UserUpdateService---');
+  const userUpdateService: UserUpdateService = container.resolve(
+    UserUpdateService,
+  );
+  const userUpdateCommand = new UserUpdateCommand('2', 'NewName');
+
+  try {
+    await userUpdateService.handle(userUpdateCommand);
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log('---UserDeleteService---');
+  const userDeleteService: UserDeleteService = container.resolve(
+    UserDeleteService,
+  );
+  const userDeleteCommand = new UserDeleteCommand('2');
+
+  try {
+    await userDeleteService.handle(userDeleteCommand);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 main();
-
-// console.log('---UserGetInfoService---');
-// const userGetInfoService: UserGetInfoService = container.resolve(
-//   UserGetInfoService,
-// );
-// userGetInfoService.handle('1');
-
-// console.log('---UserUpdateService---');
-// const userUpdateService: UserUpdateService = container.resolve(
-//   UserUpdateService,
-// );
-// const userUpdateCommand = new UserUpdateCommand('2', 'NewName');
-// userUpdateService.handle(userUpdateCommand);
-
-// console.log('---UserDeleteService---');
-// const userDeleteService: UserDeleteService = container.resolve(
-//   UserDeleteService,
-// );
-// const userDeleteCommand = new UserDeleteCommand('2');
-// userDeleteService.handle(userDeleteCommand);
